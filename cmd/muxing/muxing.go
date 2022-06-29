bc8026724a556f70068d259487c650b483fd807c
mishanko <mishajudoist@gmail.com>
2022-06-29 22:52:36 +0300

    done both gets

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -17,9 +17,36 @@ Feel free to drop gorilla.mux if you want and use any other solution available.
 main function reads host/port from env just for an example, flavor it following your taste
 */
 
+func HelloGet(w http.ResponseWriter, r *http.Request) {
+
+	param, _ := mux.Vars(r)["PARAM"]
+
+	_, err := fmt.Fprintf(w, "Hello, %s", param)
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+}
+
+func BadGet(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+	//w.Header().Set("Content-Type", "application/json")
+	//resp := make(map[string]string)
+	//resp["message"] = "Some Error Occurred"
+	//jsonResp, err := json.Marshal(resp)
+	//if err != nil {
+	//	log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+	//}
+	//w.Write(jsonResp)
+	return
+}
+
 // Start /** Starts the web server listener on given host and port.
 func Start(host string, port int) {
 	router := mux.NewRouter()
+
+	router.HandleFunc("/name/{PARAM}", HelloGet).Methods(http.MethodGet)
+	router.HandleFunc("/bad", BadGet).Methods(http.MethodGet)
 
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
